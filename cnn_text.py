dfd462bf4424583d19161b52c0bf61849544ce4d
md.rasheduzzaman <rashed091nsu@gmail.com>
2018-02-09 22:07:35 +0600

    Add cnn text classification code

---
+++ cnn_text.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import, division, print_function
+
+import numpy as np
+import tensorflow as tf
+
+
+class TestCNN():
+    def __init__(self, sequence_length, num_classes, vocab_size, embedding_size, filter_sizes, num_filters):
+        pass
