b7ce8271e79f92a3ed86030a797955d6abfe23c8
mrasheduzzaman <rashed091nsu@gmail.com>
2018-05-25 13:52:58 +0600

    Add gpu module and test code

--- test.py
+++ test.py
@@ -8,3 +8,12 @@ import tensorflow as tf
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 print("Tensorflow version " + tf.__version__)
+
+#Creates a graph.
+a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+c = tf.matmul(a, b)
+# Creates a session with log_device_placement set to True.
+sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+# Runs the op.
+print(sess.run(c))
